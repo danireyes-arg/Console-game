71977b08042dc6e36baaf4ff84af31345eed7b5d
danireyes-arg <danielreyes.arg@gmail.com>
2022-12-19 19:25:56 -0300

    slight changes

--- src/js/code.js
+++ src/js/code.js
@@ -1,5 +1,5 @@
-function game(hand){
-    let screen = document.getElementById('screen');
+const game = (hand) => {
+    const screen = document.getElementById('screen');
     const ops = ['piedra', 'papel', 'tijera']
     const bot = ops.at(Math.floor(Math.random() * ops.length))
     if(hand === bot) screen.innerText = `${hand} vs ${bot}\n` + '> E M P A T E'
@@ -8,4 +8,3 @@ function game(hand){
     else if(hand === ops[2] && bot !== ops[0]) screen.innerText = `${hand} vs ${bot}\n` + '> G A N A S T E'
     else{ screen.innerText = `${hand} vs  ${bot}\n` + '> P E R D I S T E' }
 }
-console.log(game('tijera'))
